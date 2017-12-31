5859d7f603163818cd1b42b0ce0d49d75c44c1d5
wangyang <desolator@sina.com>
2017-12-31 21:49:25 +0800

    use react antd

--- taijun/settings.py
+++ taijun/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
+    'taijun'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'taijun.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'taijun',
+        'USER': 'root',
+        'PASSWORD': 'a1234567!',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
